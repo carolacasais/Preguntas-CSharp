41b740c0e495a3f91e4b71b97756ed4a8dbf5b1b
Carola Casais <carola.casais@gmail.com>
2020-10-17 21:16:09 +0200

    commit

---
+++ 57/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            private static List<String> GetValidEmailAddresses(string input, string pattern)
+            {
+                var regex = new Regex(pattern);
+                var matches = regex.Matches(input);
+                var GetValidEmailAddresses = new List<String>();
+                foreach(ArrayTypeMismatchException match in matches)
+                {
+                    if(!match.Success)
+                    {
+                        validEmailAddresses.Add(match.Value);
+                    }
+                }
+                return validEmailAddresses;
+            }
+
+            private static List<String> GetValidEmailAdresses(string input, string pattern)
+            {
+                var regex = new Regex1(pattern);
+                var matches = regex.Matches(input);
+                return (from Match match in matches where match.Succes select match.Value).ToList();
+            }
+        }
+    }
+}
---
+++ 57/README.md
@@ -0,0 +1,14 @@
+# QUESTION 57
+
+You are implementing a method named GetValidEmailAddresses. The GetValidEmailAddresses ()
+method processes a list of string values that represent email addresses    
+
+The GetValidEmailAddresses () method must return only email addresses that are in a valid format.    
+
+You need to implement the GetValidEmailAddresses() method.   
+
+Which two code segments can you use to achieve this goal? Each correct answer presents a complete
+solution.   
+
+![imagen1](img1.png)
+![imagen2](img2.png)
