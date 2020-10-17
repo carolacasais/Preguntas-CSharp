80eeb28f16f4bd4124088b2431abee4f525fa7e8
Carola Casais <carola.casais@gmail.com>
2020-10-17 21:39:33 +0200

    commit

---
+++ 77/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _77
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 77/README.md
@@ -0,0 +1,6 @@
+# QUESTION 77
+
+![imagen1](img1.png)
+![imagen2](img2.png)
+
+**CORRECT ANSWER: B**   
