c3bd2e9b2243409d1682da9b913da8892cf3a00f
Carola Casais <carola.casais@gmail.com>
2020-10-17 19:34:54 +0200

    first commit

---
+++ 9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _9
+{
+    class Program
+    {
+        //enum Cardinales {North, South, East, West};
+        static void Main(string[] args)
+        {
+            public enum Compass
+            {
+                North,
+                South,
+                East,
+                West,
+            }
+            [DataContract]
+            public class Location
+            {
+                [DataMember]
+                public string Label { get; set; }
+                [DataMember]
+                public Compass Direction { get; set; }
+            }
+            void DoWork()
+            {
+                var location = new Location { Label = "Test", Direction = Compass.West };
+                Console.WriteLine(WriteObject(location,
+                // AQUI INSERTAR CODIGO
+                New DataContractJsonSerializer(typeof(Location))
+                ));
+            }
+        }
+    }
+}
---
+++ 9/README.md
@@ -0,0 +1,9 @@
+# QUESTION 9
+
+You are developing an application. The application converts a Location object to a string by using a method
+named WriteObject. The WriteObject() method accepts two parameters, a Location object and an
+XmlObjectSerializer object.      
+The application includes the following code. (Line numbers are included for reference only.)    
+
+![imagen](img1.png)
+
