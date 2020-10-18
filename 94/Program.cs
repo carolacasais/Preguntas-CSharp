f37f0b83164771217be611d59313035170dad775
Carola Casais <carola.casais@gmail.com>
2020-10-18 19:40:32 +0200

    commit

---
+++ 94/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _94
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            private bool IsNull (object obj)
+            {
+                if (obj == null)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+        }
+    }
+}
---
+++ 94/README.md
@@ -0,0 +1,12 @@
+# QUESTION 94
+
+You are developing an application that uses several objects. The application includes the following code
+segment. (Line numbers are included for reference only.)    
+
+![imagen1](img1.png)
+
+You need to evaluate whether an object is null.   
+Which code segment should you insert at line 03?    
+
+![imagen2](img2.png)
+
