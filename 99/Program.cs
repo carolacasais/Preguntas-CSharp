5b5d9a1a35773f16371eeddfe59150b978eefc23
Carola Casais <carola.casais@gmail.com>
2020-10-18 20:01:35 +0200

    commit

---
+++ 99/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _99
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            private string GetMemberType(int memberCode)
+            {
+                string memberType;
+                switch (memberCode)
+                {
+                    case 0:
+                        memberType = "Non-Member";
+                        break;
+                    case 1:
+                        memberType = "Member";
+                        break;
+                    default:
+                        memberType = "Invalid";
+                        break;
+                }
+                return memberType;
+                }
+            }
+        }
+    }
+}
