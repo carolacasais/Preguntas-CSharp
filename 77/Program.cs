4c0edbfb83607f2360d6538d22440ffcfadf5e56
Carola Casais <carola.casais@gmail.com>
2020-10-18 19:34:09 +0200

    commit

--- 77/Program.cs
+++ 77/Program.cs
@@ -6,7 +6,20 @@ namespace _77
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            public class Product
+            {
+                public int Id { get; set; }
+                public int CategoryId { get; set; }
+                public string Name { get; set; }
+                public bool IsValid { get; set; }
+            }
+            public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+            {
+                if (IDisposable <= 0)
+                    yield return new ValidationResult("Product Od is required.", new[] {"Id"});
+                if (string.IsNullOrEmpty(Name))
+                    yield return new ValidationResult("Product Od is required.", new[] {"Name"});
+            }
         }
     }
 }
