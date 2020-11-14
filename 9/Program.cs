c6211d9468254e9b993f7147ca94941b4dbe2e50
Carola Casais <carola.casais@gmail.com>
2020-11-14 13:09:51 +0100

    commit

--- 9/Program.cs
+++ 9/Program.cs
@@ -1,71 +1,53 @@
 ﻿using System;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
+using System.IO;
 
-namespace _9
+namespace _9b
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            //new Program().DoWork();
+            Program pr = new Program();
+            pr.DoWork();
         }
-    }
-
-    public enum Compass
-            {
-                North,
-                South,
-                East,
-                West,
-            }
-            [DataContract]
-            public class Location
-            {
-                [DataMember]
-                public string Label { get; set; }
-                [DataMember]
-                public Compass Direction { get; set; }
-            }
-
-            public override void WriteObject (System.Xml.XmlWriter writer, object? graph); // esto es lo que añadí para que funcione
-            {
-                
-                public class Test
-                {
-                    private void WriteObjectWithInstance(XmlObjectSerializer xm, Location graph,
-                    string fileName)
-                    {
-                        // Use either the XmlDataContractSerializer or NetDataContractSerializer,
-                        // or any other class that inherits from XmlObjectSerializer to write with.
-                        Console.WriteLine(xm.GetType());
-                        FileStream fs = new FileStream(fileName, FileMode.Create);
-                        XmlDictionaryWriter writer = XmlDictionaryWriter.CreateTextWriter(fs);
-                        xm.WriteObject(writer, graph);
-                        Console.WriteLine("Done writing {0}", fileName);
-                    }
-
-                    void DoWork() // de aqui hacia abajo está bien
-                    {
-                            var location = new Location { Label = "Test", Direction = Compass.West };
-                            Console.WriteLine(WriteObject(location,
-                            new DataContractJsonSerializer(typeof(Location)) // ESTE ES EL CODIGO INSERTADO
-                            )); 
-                    }
-
-                }
-            }
-
-
-            
-}
-
-
- var location = new Location { Label = "Test", Direction = Compass.West };
-            WriteLine(WriteObject(location,
-            new DataContractJsonSerializer(typeof(Location)) // ESTE ES EL CODIGO INSERTADO y el correcto :)
+        void DoWork() // de aqui hacia abajo está bien
+        {
+            var location = new Location { Label = "Test", Direction = Compass.West };
+            Console.WriteLine(WriteObject(location,
+            new DataContractJsonSerializer(typeof(Location)) // RESPUESTA CORRECTA 
             //new DataContractSerializer(typeof(Location)) // No es JSON - No funciona - Serializa una instancia de un tipo en un flujo o en un documento XML
             //new XmlSerializer(typeof(Location)) // No hereda de XmlObjectSerializer
-            //new System.Runtime.Serialization.NetDataContractSerializer() // No hereda de XmlObjectSerializer
-));
+            //new System.Runtime.Serialization.NetDataContractSerializer() // No hereda de XmlObjectSerializer // ESTE ES EL CODIGO INSERTADO
+            ));
 
+        }
+        public static string WriteObject(Location miL, XmlObjectSerializer miOS)
+        {
+            MemoryStream ms = new MemoryStream();
+            miOS.WriteObject(ms, miL);
+            byte[] cadena = ms.ToArray();
+            ms.Close();
+
+             return System.Text.Encoding.Default.GetString(cadena);
+        }
+    }
+    public enum Compass
+    {
+        North,
+        South,
+        East,
+        West,
+    }
+    [DataContract]
+    public class Location
+    {
+        [DataMember]
+        public string Label { get; set; }
+        [DataMember]
+        public Compass Direction { get; set; }
+
+    }
+}
