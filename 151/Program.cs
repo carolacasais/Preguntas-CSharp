9485023f846105840bfb24b896dd8151e1edc661
Carola Casais <carola.casais@gmail.com>
2020-11-18 14:49:26 +0100

    commit

--- 151/Program.cs
+++ 151/Program.cs
@@ -1,4 +1,13 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Net;
+using System.Web;
+
 
 namespace _151
 {
@@ -6,7 +15,41 @@ namespace _151
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program pr = new Program();
+            pr.DoWork();
+
         }
+
+        void DoWork()
+        {
+            //static void ExecuteSqlTransaction(string connectionString)
+            
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand("procl", connection);
+
+                SqlTransaction transaction = connection.BeginTransaction(System.Data.IsolationLevel.RepeatableRead);
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                }
+                finally
+                {
+                    commmand.Dispose();
+                    connection.Dispose();
+                }
+
+            
+        }
+
+
     }
+
 }
