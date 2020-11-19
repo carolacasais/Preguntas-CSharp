0316e61adaa17f8c24eed466a351231d23e617ea
Carola Casais <carola.casais@gmail.com>
2020-11-19 12:51:26 +0100

    commit

--- 57/Program.cs
+++ 57/Program.cs
@@ -36,14 +36,36 @@ namespace _57
                     validEmailAddresses.Add(match.Value);
                 }
             }
-            return validEmailAddresses;
-        }
+            return GetValidEmailAddresses;
+        } // B
 
             /*private static List<String> GetValidEmailAdresses(string input, string pattern)
             {
                 var regex = new Regex1(pattern);
                 var matches = regex.Matches(input);
                 return (from Match match in matches where match.Succes select match.Value).ToList();
-            }*/
+            } C */
+
+             /*private static List<String> GetValidEmailAdresses(string input, string pattern)
+            {
+                var regex = new Regex1(pattern);
+                var matches = regex.Matches(input);
+                return (from Match match in matches where match.Succes select match.Success.ToSring()).ToList();
+            } D */
+
+            /*private static List<String> GetValidEmailAddresses(string input, string pattern)
+            {
+                var regex = new Regex(pattern);
+                var matches = regex.Matches(input);
+                var GetValidEmailAddresses = new List<String>();
+                foreach(ArrayTypeMismatchException match in matches)
+                {
+                    if(match.Success)
+                    {
+                        validEmailAddresses.Add(match.Value);
+                    }
+                }
+                return GetValidEmailAddresses;
+            } A */
     }
 }
