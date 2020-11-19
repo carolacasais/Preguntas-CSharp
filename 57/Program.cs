1a5c742e12008ce17c4b0a4866131e03b01c4a8d
Carola Casais <carola.casais@gmail.com>
2020-11-19 12:55:36 +0100

    commit

--- 57/Program.cs
+++ 57/Program.cs
@@ -17,8 +17,9 @@ namespace _57
         static void Main(string[] args)
         {
             //Console.WriteLine(GetValidEmailAddresses());
-            Program pr = new Program(); //creating object of class Program
-            pr.GetValidEmailAddresses(); // Calling method
+            //Program pr = new Program(); //creating object of class Program
+            //pr.GetValidEmailAddresses(); // Calling method
+            List<String> GetValidEmailAddresses = new List<String>();
     
 
             
