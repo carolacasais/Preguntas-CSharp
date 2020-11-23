87c892497db4c408f5e765bbe54833b7e9be4306
Carola Casais <carola.casais@gmail.com>
2020-11-23 10:38:35 +0100

    commit

---
+++ 153/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _153
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            pr.DoWork();
+        }
+
+        public void DoWork();
+        {
+            Int32 returnStatus = Int32.MinValue;
+            switch (status)
+            {
+                case "Active":
+                returnStatus = 1;
+                break;
+                case "Inactive":
+                returnStatus = 0;
+                break;
+                default:
+                returnStatus = -1;
+                break;
+                
+            }
+            return returnStatus;
+        }
+    }
+}
