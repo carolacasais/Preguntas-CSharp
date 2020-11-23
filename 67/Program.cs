f9cf1baf4789b9646e8d5aab072a5ff41b038141
Carola Casais <carola.casais@gmail.com>
2020-11-23 19:38:01 +0100

    commit

--- 67/Program.cs
+++ 67/Program.cs
@@ -15,10 +15,44 @@ namespace _67
     {
         static void Main(string[] args)
         {
-            double x, y;
+            double x, y; 
             x = 0.0;
             y = 0.0;
-            Console.WriteLine(x/y);
+            
+            Console.WriteLine($"Dividir entre cero Double:{x/y}"); 
+
+
+            float a,b;
+            a = 0.0f;
+            b = 0.0f;
+            Console.WriteLine($"Dividir entre cero float:{a/b}"); 
+
+            int z,w;
+            z = 0;
+            w = 0;
+            try
+            {
+                Console.WriteLine(z/w);
+            }
+            catch (System.Exception e)
+            {
+                
+                Console.WriteLine($"Dividir entre cero int:{e.Message}"); 
+            }
+
+            decimal l,m;
+            l = 0.0m;
+            m = 0.0m;
+            try
+            {
+                Console.WriteLine(l/m);
+            }
+            catch (System.Exception e)
+            {
+                
+                Console.WriteLine($"Dividir entre cero decimal:{e.Message}"); 
+            }
+            
         }
     }
 }
