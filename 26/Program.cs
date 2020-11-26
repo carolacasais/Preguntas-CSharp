39fe2be017c4e181ba5a3eeae91a19be712d136e
Carola Casais <carola.casais@gmail.com>
2020-11-26 14:29:11 +0100

    commit

--- 26/Program.cs
+++ 26/Program.cs
@@ -1,32 +1,33 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
-using System.Xml;
 using System.IO;
-using System.Runtime.Serialization.Json;
 using System.Net;
-using System.Web;
-using System.Data.SqlClient;
-using System.Threading;
+
 
 namespace _26
 {
     class Program
     {
+        private async void GetData(WebResponse response)
+        {
+            var streamreader = new StreamReader(response.GetResponseStream());
+            //var utlTextText = await streamReader.ReadLineAsync(); estp es lo que proponía la batería de preguntas
+            Console.WriteLine (await streamreader.ReadLineAsync());
+            streamreader.Close();
+            // El porque ReadLineAsync() y no ReadToEndAsync() es por the method must return a string value that contains the first line
+        }
         static void Main(string[] args)
         {
-            //Program p = new Program(); //creating object of class Program
-            //p.GetData(new object()); // Calling method
-            //GetData();
-            var response = GetData(WebResponse);
-            Console.WriteLine(response.Result);
+            Console.WriteLine("este ejemplo va a escribir la primera linea de la pagina https://dotnetfoundation.org/; ");
+            WebRequest myWebRequest = WebRequest.Create("https://dotnetfoundation.org/");
+            WebResponse myWebResponse = myWebRequest.GetResponse();
             
+            //new Program().GetData(myWebResponse);
+            Program pr = new Program();
+            pr.GetData(myWebResponse); // le llamamos asi porque main es static y GetData no
+
+            myWebResponse.Close();
+            Console.WriteLine("Claro : <!DOCTYPE html> que te esperabas ;)");
         }
-        private async void GetData(WebResponse response)
-            {
-                var streamReader = new StreamReader(response.GetResponseStream());
-                var utlTextText = await streamReader.ReadLineAsync();
-            }
+
     }
 }
