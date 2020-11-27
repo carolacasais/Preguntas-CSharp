e55c5c40773741c9f7af8c7f986ed4666733c3fd
Carola Casais <carola.casais@gmail.com>
2020-11-27 19:29:31 +0100

    commit

--- 57/Program.cs
+++ 57/Program.cs
@@ -19,9 +19,14 @@ namespace _57
             //Console.WriteLine(GetValidEmailAddresses());
             //Program pr = new Program(); //creating object of class Program
             //pr.GetValidEmailAddresses(); // Calling method
-            List<String> GetValidEmailAddresses = new List<String>();
-    
-
+            string ListaEmails = "ahotmail.com";
+            string pattern = @"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{​​\|}​​~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{​​\|}​​~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$";
+            List<String> Lista = GetValidEmailAddresses(ListaEmails, pattern);
+           Console.WriteLine("probando opcion 1!");
+            foreach(var item in Lista)
+            {
+                Console.WriteLine(item);
+            }
             
         }
         
@@ -29,16 +34,16 @@ namespace _57
         {
             var regex = new Regex(pattern);
             var matches = regex.Matches(input);
-            var GetValidEmailAddresses = new List<String>();
-            foreach(ArrayTypeMismatchException match in matches)
+            var ValidEmailAddresses = new List<String>();
+            foreach(Match match in matches)
             {
                 if(!match.Success)
                 {
-                    validEmailAddresses.Add(match.Value);
+                    ValidEmailAddresses.Add(match.Value);
                 }
             }
-            return GetValidEmailAddresses;
-        } // B
+            return ValidEmailAddresses;
+        } // B 
 
             /*private static List<String> GetValidEmailAdresses(string input, string pattern)
             {
@@ -70,3 +75,5 @@ namespace _57
             } A */
     }
 }
+
+
